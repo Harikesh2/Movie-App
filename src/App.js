74c7ad8bcc02e3deb8a0860dd87c9b630ba74271
Harikesh2 <mths83478@gmail.com>
2023-10-28 22:02:24 +0530

    Added index.css and moviecard!!

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import MovieCard from "./moviecard";
+
 function App() {
   return (  
    <>
-   <h1>Movie App</h1>
-   
    <MovieCard />
    </>
   );
--- src/moviecard.js
+++ src/moviecard.js
@@ -1,5 +1,5 @@
 import {Component} from "react";
-import "./index.css";
+
 
 class MovieCard extends Component{
     render(){
@@ -7,24 +7,35 @@ class MovieCard extends Component{
             <div className="main">
               <div className="movie-card">
                 <div className="left">
-                    <img alt="poster"/>
+                        <img alt="poster" src='https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg' />
+                        </div>
                     <div className="right">
                         <div className="title">
                             The Avenger
                         </div>
                         <div className="plot">
-                           Super natural power in the movie shown!!
+                        Earth's mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity.  
                         </div>
-                        <div className="price">
-                           Super natural power in the movie shown!!
-                        </div>
+                        <div className="price">RS. 199</div>
                         <div className="footer">
-                            <div className="rating">
-                                8.9
+                            <div className="rating">8.9</div>
+
+                            <div className="star-dis">
+                            <img className="str-btn" 
+                                    alt="Decrease" 
+                                    src="https://cdn-icons-png.flaticon.com/128/2801/2801932.png" 
+                                />
+                                <img className="stars" 
+                                        alt="stars" 
+                                        src="https://cdn-icons-png.flaticon.com/128/2107/2107957.png"    
+                                />
+                                <img className="str-btn" 
+                                    alt="increase" 
+                                    src="https://cdn-icons-png.flaticon.com/128/2997/2997933.png" 
+                                />
+                                <span className="starCount">0</span> 
                             </div>
-                            <div className="stars">
-                                Star
-                            </div>
+
                             <button className="favourite-btn">Favourite</button>
                             <button className="cart-btn">Add to Cart</button>
                         </div>
@@ -32,7 +43,7 @@ class MovieCard extends Component{
 
                     </div>
 
-                </div>
+               
               </div>   
             </div>
         )
